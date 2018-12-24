a0f872f71b9256e51817f87765b005580c598d82
かっしー <kasikasi00@gmail.com>
2018-12-25 00:49:43 +0900

    chapter 5 finish

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,23 @@ module Taskleaf
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = 'Tokyo'
+
+    config.generators do |g|
+      g.system_tests false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+
+      g.test_framework :rspec,
+                        fixtures: false,
+                        view_specs: false,
+                        helper_specs: false,
+                        routing_specs: false,
+                        controller_specs: false,
+                        request_specs: true,
+                        system_specs: true
+    end
   end
 end
